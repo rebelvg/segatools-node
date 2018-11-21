2ff77ba472553669bc99f276b3476f298e00bc75
SlowpokeVG <screamer.jr@gmail.com>
2018-11-21 11:33:47 +0300

    added escape regex for search

--- services/messages/index.js
+++ services/messages/index.js
@@ -6,7 +6,8 @@ module.exports = async (fastify, opts) => {
 		const limit = _.toInteger(req.query.limit) || 10 // google fastify request. Here we are taking query params and if there are no query - we use default
 		const page = _.toInteger(req.query.page) || 1
 		const count = await cursor.count(); //get messages count
-		const result = await cursor.find({}).sort({timestamp: -1}).skip((limit * (page - 1)) - (page-1)).limit(limit).toArray();// we take all items, sort them, limit them, skip a limited and return array
+		const searchregex = new RegExp(_.escapeRegExp(req.query.search), "i")
+		const result = await cursor.find( { $or: [ {"English": searchregex} , {"Japanese": searchregex} ]}).sort({timestamp: -1}).skip((limit * (page - 1)) - (page-1)).limit(limit).toArray();// we take all items, sort them, limit them, skip a limited and return array
 		const info = { current_page: page, all_pages: _.ceil(count / limit), all_results: count }; // data for pagination
 		res.send({ messages: result, info }) // send result
 	})
--- services/names/index.js
+++ services/names/index.js
@@ -3,9 +3,8 @@ const _ = require('lodash')
 module.exports = async (fastify, opts) => {
 	fastify.get('/', async function (req, res) {
 		const cursor = this.mongo.db.collection('names') //connect to collection
-		const limit = _.toInteger(req.query.filter)
-		var foundnames = await cursor.find( { $or: [ {"English": new RegExp(req.query.filter, "i")} , {"Japanese": new RegExp(req.query.filter, "i")} ]})
-		const result = await foundnames.sort({timestamp: -1}).toArray();// we take all items, limit them, skip a limited and return array
+		const searchregex = new RegExp(_.escapeRegExp(req.query.search), "i")
+		const result = await cursor.find( { $or: [ {"English": searchregex} , {"Japanese": searchregex} ]}).sort({timestamp: -1}).toArray();
 		res.send({ names: result }) // send result
 	})
 }
