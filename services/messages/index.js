231cff74ced9c5e5754c60f3eed7ffce5a22fef8
SlowpokeVG <screamer.jr@gmail.com>
2018-11-23 11:13:36 +0300

    added search by specific names

--- services/messages/index.js
+++ services/messages/index.js
@@ -6,30 +6,32 @@ module.exports = async (fastify, opts) => {
 		const limit = _.toInteger(req.query.limit) || 10 // google fastify request. Here we are taking query params and if there are no query - we use default
 		const page = _.toInteger(req.query.page) || 1
 
-		var sortingstring = 'timestamp'
-		var sortorder = -1
+		var sortingstring = 'timestamp' // default sorting by timestamp
+		var sortorder = -1 //descending order by default
 
-		if (req.query.sort_by) {sortingstring=req.query.sort_by}
-		if (req.query.sort_order == "asc") {sortorder = 1}
+		if (req.query.sort_by) {sortingstring=req.query.sort_by} //if set - sort by this
+		if (req.query.sort_order == "asc") {sortorder = 1} //ascention order if needed
 
-		var query = {}
+		var query = {"$and":[]} //couldn't find a way to push in query easier
+		
 		if (req.query.search) 
-		{
+		{	//search both japanese and english, creating regexp that searches for every word separately
 			const searchregex = new RegExp(_.escapeRegExp(req.query.search).replace(/^/i,"(?=.*").replace(/\s/g,")(?=.*").replace(/$/i,")"), "i");
-			query["$or"] =[{'English': searchregex},{'Japanese': searchregex}];
+			query["$and"].push({$or:[{'English': searchregex},{'Japanese': searchregex}] });
 		}
-		if (req.query.chapter == "No Chapter") query["$or"] = [{'chapter': req.query.chapter},{'chapter': { $exists: false }}]
-		else if (req.query.chapter) query["$and"] = [{'chapter': req.query.chapter},{'chapter': { $exists: true }}]
-		if (req.query.filename) query["Filename"] = new RegExp(_.escapeRegExp(req.query.filename).replace(/\s/g,""), "i")
-		if (req.query.speakers_count) query["nameIDs"] = {$size: _.toInteger(req.query.speakers_count)}
-		//if (req.query.hide_changed) 
-		//if (req.query.hide_completed) query["$where"] = "function() { return this.English.length != this.Japanese.length}" 
+		
+		if (req.query.chapter == "No Chapter") query["$and"].push({$or:[{'chapter': req.query.chapter},{'chapter': { $exists: false }}]}) //if no chapter - search "no chapter" and items without chapter
+		else if (req.query.chapter) query["$and"].push( {'chapter': req.query.chapter} ,{'chapter': { $exists: true }})	//if any other chapter - search for it and don't count items without chapter
+		if (req.query.filename) query["$and"].push({"Filename": new RegExp(_.escapeRegExp(req.query.filename).replace(/\s/g,""), "i")}) //filename search, removes spaces
+		if (req.query.speakers_count) query["$and"].push({"nameIDs": {$size: _.toInteger(req.query.speakers_count)}}) //search by speaker count
+		if (req.query.speakers) query["$and"].push({"nameIDs": { "$all": req.query.speakers.split(',').map(Number) } }) //search by names separated by commas
+		if (query["$and"].length == 0) query = {} //if "#and" is empty remove it to prevent error
 
-		const foundmessages = await cursor.find(query)		
+		const foundmessages = await cursor.find(query)
 		const count = await foundmessages.count();
 		const result = await foundmessages.sort({[sortingstring]: sortorder}).skip((limit * (page - 1)) - (page-1)).limit(limit).toArray();// we take all items, sort them, limit them, skip a limited and return array
 		const info = { current_page: page, all_pages: _.ceil(count / limit), all_results: count }; // data for pagination
-		res.send({ info, messages: result }) // send result
+		res.send({ info, ass:query, messages: result }) // send result
 	})
   
 
