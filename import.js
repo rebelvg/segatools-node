ef1c4afb945d0d4993ad1d6c89e7ad8f5686d5f2
SlowpokeVG <screamer.jr@gmail.com>
2018-11-25 08:24:14 +0300

    fixed database, implemented all features for search
    - import.js now adds missing message ids in database before import
    - hide_changed and hide_completed functions available
    - output json now has progress percentage and names array in it

--- import.js
+++ import.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-
+const _ = require('lodash')
 const MongoClient = require('mongodb').MongoClient;
 const objectID = require('mongodb').ObjectID
 const url = 'mongodb://localhost';
@@ -7,19 +7,26 @@ const dbName = 'segatools'
 const folder = './import'
 
 MongoClient.connect(url, async function(err, client) {
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
 
-  fs.readdir(folder, (err, files) => {
-    files.forEach(file => {
-      const collection = db.collection(file.replace('.json', ''));
-      const importedData = require('./import/'+file)
-      Object.values(importedData).map(async (item) => {
-        await collection.insertOne({...item, '_id': objectID(item._id['$id'])})
-      });
-      console.log(`imported ${file}`)
-    });
-
-    client.close()
-  })
-});
+    fs.readdir(folder, (err, files) => {
+        files.forEach(file => {
+            const collection = db.collection(file.replace('.json', ''));
+            const importedData = require('./import/' + file)
+            var jsonready = []
+            Object.values(importedData).map(async (item) => jsonready.push({...item, '_id': objectID(item._id['$id'])}))
+            if (file == "messages.json")
+            {
+                jsonready.forEach(Mfile =>
+                {
+                    if (typeof Mfile.English == 'object')
+                        Mfile.English = [...Array(Mfile.Japanese.length)].map((v, i) => Mfile.English[i] || null)
+                })
+            }
+                collection.insert(jsonready)
+                console.log(`imported ${file}`)
+           });
+            client.close()
+        })
+    })
--- services/messages/index.js
+++ services/messages/index.js
@@ -9,29 +9,45 @@ module.exports = async (fastify, opts) => {
 		var sortingstring = 'timestamp' // default sorting by timestamp
 		var sortorder = -1 //descending order by default
 
-		if (req.query.sort_by) {sortingstring=req.query.sort_by} //if set - sort by this
-		if (req.query.sort_order == "asc") {sortorder = 1} //ascention order if needed
+		if (req.query.sort_by) sortingstring=req.query.sort_by //if set - sort by this
+		if (req.query.asc_order == "true") sortorder = 1 //ascension order if needed
 
 		var query = {"$and":[]} //couldn't find a way to push in query easier
-		
-		if (req.query.search) 
-		{	//search both japanese and english, creating regexp that searches for every word separately
-			const searchregex = new RegExp(_.escapeRegExp(req.query.search).replace(/^/i,"(?=.*").replace(/\s/g,")(?=.*").replace(/$/i,")"), "i");
-			query["$and"].push({$or:[{'English': searchregex},{'Japanese': searchregex}] });
-		}
-		
+
+        if (req.query.search)
+        {	//search both japanese and english, creating regexp that searches for every word separately
+            const searchregex = new RegExp(_.escapeRegExp(req.query.search).replace(/^/i,"(?=.*").replace(/\s/g,")(?=.*").replace(/$/i,")"), "i");
+            query["$and"].push({$or:[{'English': searchregex},{'Japanese': searchregex}] });
+        }
 		if (req.query.chapter == "No Chapter") query["$and"].push({$or:[{'chapter': req.query.chapter},{'chapter': { $exists: false }}]}) //if no chapter - search "no chapter" and items without chapter
 		else if (req.query.chapter) query["$and"].push( {'chapter': req.query.chapter} ,{'chapter': { $exists: true }})	//if any other chapter - search for it and don't count items without chapter
 		if (req.query.filename) query["$and"].push({"Filename": new RegExp(_.escapeRegExp(req.query.filename).replace(/\s/g,""), "i")}) //filename search, removes spaces
 		if (req.query.speakers_count) query["$and"].push({"nameIDs": {$size: _.toInteger(req.query.speakers_count)}}) //search by speaker count
 		if (req.query.speakers) query["$and"].push({"nameIDs": { "$all": req.query.speakers.split(',').map(Number) } }) //search by names separated by commas
 		if (query["$and"].length == 0) query = {} //if "#and" is empty remove it to prevent error
+        if (req.query.hide_completed) query["$where"] = "function() { return this.English.filter(e => e !== null).length != this.Japanese.filter(e => e !== '').length}"
+        if (req.query.hide_changed) query["$where"] = "function() { return this.English.filter(e => e !== null).length == 0}"
 
 		const foundmessages = await cursor.find(query)
 		const count = await foundmessages.count();
-		const result = await foundmessages.sort({[sortingstring]: sortorder}).skip((limit * (page - 1)) - (page-1)).limit(limit).toArray();// we take all items, sort them, limit them, skip a limited and return array
+
+		const names = await this.mongo.db.collection('names').find().toArray();	//get all names to put into the output
+		var result = await foundmessages.skip((limit * (page - 1)) - (page-1)).limit(limit).toArray();// we take all items, sort them, limit them, skip a limited and return array
+
+        result.forEach(function(element)
+		{	//add an array of names to output "Japanese (English)"
+        	element.names = element.nameIDs.map((function(name)
+			{
+            	Englishname = ""
+            	if (names[name].English) Englishname = " (" + names[name].English + ")"
+            	return names[name].Japanese + Englishname
+        	}))
+			//add progress percentage
+			element.progress = (element.English.filter(e => e !== null).length * 100 / element.Japanese.filter(e => e !== '').length).toFixed(1);
+		})
+
 		const info = { current_page: page, all_pages: _.ceil(count / limit), all_results: count }; // data for pagination
-		res.send({ info, ass:query, messages: result }) // send result
+		res.send({ info, messages: result }) // send result
 	})
   
 
