fac02f111b26d14ae41d96a311ad2bfbf2db7888
Rebel <johnhartigan@ya.ru>
2018-11-29 23:28:56 +0200

    Handle async errors in controllers.

--- routes/messages.js
+++ routes/messages.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
+const asyncMiddleware = require('../helpers/async-middleware');
+
 const find = require('../controllers/messages/find');
 const findById = require('../controllers/messages/findById');
 const update = require('../controllers/messages/update');
 
 const router = express.Router();
 
-router.get('/:id', findById);
-router.get('/', find);
-router.post('/:id', update);
+router.get('/:id', asyncMiddleware(findById));
+router.get('/', asyncMiddleware(find));
+router.post('/:id', asyncMiddleware(update));
 
 module.exports = router;
--- routes/names.js
+++ routes/names.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const asyncMiddleware = require('../helpers/async-middleware');
+
 const find = require('../controllers/names/find');
 
 const router = express.Router();
 
-router.get('/', find);
+router.get('/', asyncMiddleware(find));
 
 module.exports = router;
