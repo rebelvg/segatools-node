075ffe93ceaa81dbe5618c387b7e98a0000e9f96
Rebel <johnhartigan@ya.ru>
2018-11-30 21:09:16 +0200

    Improved import.

--- scripts/import-messages.js
+++ scripts/import-messages.js
@@ -10,39 +10,39 @@ const importedMessagesData = require('./import/messages.json');
 const importedSpeakersData = require('./import/speakers.json');
 
 (async () => {
-  const mongoClient = await MongoClient.connect(mongoUrl);
+  const mongoClient = await MongoClient.connect(mongoUrl, { useNewUrlParser: true });
 
   const db = mongoClient.db(dbName);
 
   const messagesCollection = db.collection('messages');
 
-  const importPromises = _.map(importedMessagesData, async (message, index) => {
+  const importPromises = _.map(importedMessagesData, async (message) => {
     const lines = [];
 
-    const speakers = _.find(importedSpeakersData, ['FileName', message.Filename]).NameIDs;
+    const speakerIds = _.find(importedSpeakersData, ['FileName', message.Filename]).NameIDs;
 
     _.forEach(message.Japanese, (japaneseLine, index) => {
       lines.push({
         text: {
-          japanese: japaneseLine,
-          english: message.English[index]
+          japanese: japaneseLine || null,
+          english: message.English[index] || null
         },
-        speakerId: speakers[index]
+        speakerId: speakerIds[index]
       });
     });
 
     return messagesCollection.insertOne({
-      _id: message._id,
+      _id: new ObjectID(message._id['$id']),
       fileName: message.Filename,
       lines: lines,
-      nameIDs: message.nameIDs,
-      timeUpdated: message.timestamp
+      nameIds: message.nameIDs,
+      timeUpdated: new Date(message.timestamp * 1000)
     });
   });
 
   await Promise.all(importPromises);
 
-  console.log('imported messages');
+  console.log('imported done.');
 
   await mongoClient.close();
 })();
