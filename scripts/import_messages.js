35d479d45e40d66e031e73f2f029ee5342d4b65a
SlowpokeVG <screamer.jr@gmail.com>
2018-11-30 20:05:44 +0300

    rearranged database on import

---
+++ scripts/import_messages.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const _ = require('lodash');
+const MongoClient = require('mongodb').MongoClient;
+const objectID = require('mongodb').ObjectID;
+const url = 'mongodb://localhost';
+const dbName = 'segatools';
+const folder = './import';
+
+MongoClient.connect(
+  url,
+  async function(err, client) {
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+        
+    const collection = db.collection('messages');
+    const importedData = require('./import/messages.json');
+    const importedNames = require('./import/namemess.json');
+
+    await Object.values(importedData).map(
+      async (item, i) => {
+        let lines = [];
+        const Speaker = await _.find(importedNames, ['FileName',item.Filename]).NameIDs;
+        Object.values(item.Japanese).map(
+          async (line, message) =>
+          {
+            await lines.push({
+              Japanese: line, 
+              English: item.English[message], 
+              Speaker: Speaker[message]
+            });
+          });
+        await collection.insertOne({ 
+          _id: objectID(item._id['$id']), 
+          Filename:item.Filename, 
+          Lines: lines, 
+          nameIDs:item.nameIDs, 
+          timestamp:item.timestamp
+        });
+      }
+    );
+
+    console.log('imported messages');
+    client.close();
+  }
+);
---
+++ scripts/import_names.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const _ = require('lodash');
+const MongoClient = require('mongodb').MongoClient;
+const objectID = require('mongodb').ObjectID;
+const url = 'mongodb://localhost';
+const dbName = 'segatools';
+const folder = './import';
+
+MongoClient.connect(
+  url,
+  async function(err, client) {
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+        
+    const collection = db.collection('names');
+    const importedData = require('./import/names.json');
+    let jsonready = [];
+    Object.values(importedData).map(
+      async item => await jsonready.push({ ...item, _id: objectID(item._id['$id']) })
+    );
+
+    await collection.insert(jsonready);
+    console.log('imported');
+    
+    client.close();
+    
+  }
+);
