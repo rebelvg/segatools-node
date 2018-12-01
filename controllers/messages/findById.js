8c7f988659aec3dcd896cad833827ca91c59e1d6
SlowpokeVG <screamer.jr@gmail.com>
2018-12-01 04:18:58 +0300

    rewrote search by id

--- controllers/messages/findById.js
+++ controllers/messages/findById.js
@@ -5,71 +5,84 @@ async function findById(req, res, next) {
 
   const cursor = mongoClient.collection('messages');
   let ObjectID = require('mongodb').ObjectID;
-  const ID = new ObjectID(req.params.id);
-  const allmessages = await cursor
-    .find()
-    .sort({ _id: 1 })
-    .toArray();
-  const idmessage = allmessages.findIndex(function(element) {
-    return element._id === req.params.id;
+  const searchID = new ObjectID(req.params.id);
+  let idMessage = await cursor.findOne({
+    _id: searchID
   });
-
-  let result;
-
-  if (idmessage !== -1) {
-    let messagesCountArray = [];
-    let idarray = [];
-    result = allmessages[idmessage];
-    //loop through all messages and count how many times each one appear
-    for (let i = 0; i < result.Japanese.length; i++) {
-      let messageCount = 0;
-      allmessages.forEach(function(file) {
-        file.Japanese.forEach(function(messinfile) {
-          if (messinfile === result.Japanese[i]) {
-            messageCount++;
-          }
-        });
-        if (file.chapter === result.chapter && i === 0) {
-          idarray.push(file._id);
-        } //get ids of all files from a chapter with file once
-      });
-      messagesCountArray.push(i + ' / ' + messageCount);
-    }
-    result.count = messagesCountArray;
-    //add previous and next id in chapter
-    let currentid = idarray.indexOf(result._id);
-    if (currentid > 0) {
-      result.prev_id = idarray[currentid - 1];
-    }
-    if (currentid < idarray.length - 1) {
-      result.next_id = idarray[currentid + 1];
-    }
-    const names = await mongoClient
-      .collection('names')
-      .find()
-      .toArray();
-    result.names = result.nameIDs.map(function(name) {
-      let Englishname = '';
-      if (names[name].English) {
-        Englishname = ' (' + names[name].English + ')';
-      }
-      return names[name].Japanese + Englishname;
-    });
-    result.SpeakerName = result.Speaker.map(function(name) {
-      let fullnames = '';
-      if (name !== null) {
-        let Englishname = '';
-        if (names[name].English) {
-          Englishname = ' (' + names[name].English + ')';
-        }
-        fullnames = names[name].Japanese + Englishname;
-      }
-      return fullnames;
-    });
-    result.progress = parseFloat(
-      ((result.English.filter(e => e !== null).length * 100) / result.Japanese.filter(e => e !== '').length).toFixed(1)
-    );
+  
+  if (!idMessage) {
+    throw new Error('Message not found.');
   }
+
+  let prev_id = await cursor
+    .find({ 
+      '_id': {'$lt': searchID},
+      'chapterName':idMessage.chapterName
+    })
+    .sort({'_id': -1})
+    .limit(1)
+    .toArray();
+
+  let next_id = await cursor
+    .find({ 
+      '_id': {'$gt': searchID},
+      'chapterName':idMessage.chapterName
+    })
+    .sort({'_id': 1})
+    .limit(1)
+    .toArray();
+
+  if (prev_id[0] !== undefined) {
+    prev_id=prev_id[0]._id;
+  } else {
+    prev_id = null;
+  }
+  if (next_id[0] !== undefined) {
+    next_id=next_id[0]._id;
+  } else {
+    next_id = null;
+  }
+
+  const names = await mongoClient
+    .collection('names')
+    .find()
+    .toArray();
+
+  const result = {
+    _id: searchID,
+    fileName: idMessage.fileName,
+    chapterName: idMessage.chapterName,
+    lines: idMessage.lines.map(line =>
+    {
+      return {
+        text: line.text, 
+        speakerName: `${
+          (line.speakerId !== null) ? 
+            `${names[line.speakerId].Japanese}${
+              (names[line.speakerId].English) ? 
+                ` (${names[line.speakerId].English
+                })` : `` 
+            }`: `System`
+        }`
+      };
+    }),
+    names: idMessage.nameIds.map(name =>
+    {
+      return `${
+        (name !== null) ? 
+          `${names[name].Japanese}${
+            (names[name].English) ? 
+              ` (${names[name].English
+              })` : `` 
+          }`:''
+      }`;
+    }),
+    prev_id: prev_id,
+    next_id: next_id,
+    percentDone: idMessage.percentDone,
+    timeUpdated: idMessage.timeUpdated
+  };
+  
   res.send(result);
 }
 
