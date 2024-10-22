f81dae2aeadb2812c3d97bac532708923df78bfd
Volodymyr D. <9673665+rebelvg@users.noreply.github.com>
2024-10-22 15:33:01 +0200

    support unix socket

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { app } from './app';
 import { getMongoClient } from './mongo';
 import './passport';
+import * as fs from 'fs';
 
 import { env } from './env';
 
@@ -8,10 +9,22 @@ process.on('unhandledRejection', (reason, p) => {
   throw reason;
 });
 
+// remove previous unix socket
+if (typeof env.SERVER.PORT === 'string') {
+  if (fs.existsSync(env.SERVER.PORT)) {
+    fs.unlinkSync(env.SERVER.PORT);
+  }
+}
+
 (async () => {
   await getMongoClient();
 
   app.listen(env.SERVER.PORT, () => {
     console.log('server is running...');
+
+    // set unix socket rw rights for nginx
+    if (typeof env.SERVER.PORT === 'string') {
+      fs.chmodSync(env.SERVER.PORT, '777');
+    }
   });
 })();
